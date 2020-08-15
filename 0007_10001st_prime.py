94abbfd34ec8b14d7f1066f52a2aa8ebd6d227ba
Justin Cantrall <jcntrl@gmail.com>
2020-08-15 09:21:03 -0600

    fasterified it!

--- 0007_10001st_prime.py
+++ 0007_10001st_prime.py
@@ -6,7 +6,7 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 
 What is the 10 001st prime number?
 """
-
+import time
 def nPrimes(n):
     Primes = [2, 3]
     keepgoing = True
@@ -14,7 +14,9 @@ def nPrimes(n):
     while keepgoing:
         prmflag = True
         for prm in Primes:
-            if num % prm == 0:
+            if prm > 9 and prm ** 2 > num:
+                break #potential prime must be less than the square root of the num, has some false negatives in small numbers though
+            elif num % prm == 0:
                 prmflag = False
                 break #no need to keep checking when a nonprime is found
         if prmflag:
@@ -25,4 +27,9 @@ def nPrimes(n):
 
     return Primes
 
-print(nPrimes(10_001))
+n = 10_001
+t0= time.time()
+x = nPrimes(n)
+t1 = time.time()
+useroutput = "{}'th prime: {}. Computation completed in {} seconds."
+print(useroutput.format(n, x[-1], t1-t0))
